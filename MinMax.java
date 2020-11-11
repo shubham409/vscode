e05984c1088883d9254e03fccc049c45f3a4ddf9
shubham409 <sm091274@gmail.com>
2020-11-12 00:24:15 +0530

    adding more data types

---
+++ MinMax.java
@@ -0,0 +1,19 @@
+public class MinMax {
+    public static void main(String[] args) {
+
+        String s = "google";
+        Integer i = 5757;
+        Float f = 959.606f;
+        Short s2 = 859;
+
+        Double d = 9590.9696;
+        System.out.printf("%1$s %1$s", s);
+
+        System.out.printf("%1$10d  %2$18f", i, d);
+
+        System.out.printf("%1$s %1$s", s);
+
+        System.out.printf("%1$s %1$s", s);
+
+    }
+}
--- StringFormat.java
+++ StringFormat.java
@@ -2,12 +2,23 @@ public class StringFormat {
 
     public static void main(String[] args) {
 
+        Byte b1=127;
+        Short s1= 859;
+        Integer i1 = 5757;
+        Long l1 = (long)595858;
+        Long l2 = 595858l;
+        Long l3 = 595858L;
+
+        Float f1=959.606f;
+        Float f2 = 959.606F;
+        Double d1 = 9590.9696d;
+        Double d2 = 9590.9696D;
+
+
+        Character c1=65;
+        Character c2 = 65;
         String s= "google";
-        Integer i = 5757;
-        Float f=959.606f;
-        Short s2= 859;
         
-        Double d = 9590.9696;
         System.out.printf("%1$s %1$s", s);
 
         System.out.printf("%1$10d  %2$18f",i,d);
