aebb10463247bff30fd4ae6d42414d41dba0c44c
shubham409 <sm091274@gmail.com>
2020-11-11 21:47:04 +0530

    this

--- StringFormat.java
+++ StringFormat.java
@@ -3,8 +3,18 @@ public class StringFormat {
     public static void main(String[] args) {
 
         String s= "google";
+        Integer i = 5757;
+        Float f=959.606f;
+        Short s2= 859;
         
-        System.out.printf("%s", s);
+        Double d = 9590.9696;
+        System.out.printf("%1$s %1$s", s);
+
+        System.out.printf("%1$10d  %2$18f",i,d);
+
+        System.out.printf("%1$s %1$s", s);
+
+        System.out.printf("%1$s %1$s", s);
 
     }
 }
