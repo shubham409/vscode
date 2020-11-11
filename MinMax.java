6faecc84f06d2e0c32b62b7b858103ceb3e5f21a
shubham409 <sm091274@gmail.com>
2020-11-12 00:59:20 +0530

    Minmax Changed

--- MinMax.java
+++ MinMax.java
@@ -1,19 +1,77 @@
 public class MinMax {
     public static void main(String[] args) {
 
-        String s = "google";
-        Integer i = 5757;
-        Float f = 959.606f;
-        Short s2 = 859;
+        System.out.println("Max Value Byte "+Byte.MAX_VALUE);
+        System.out.println("Min Value Byte "+Byte.MIN_VALUE);
+        System.out.println("No of Bytes Byte "+Byte.BYTES);
+        System.out.println("No of Bits Byte "+Byte.SIZE);
 
-        Double d = 9590.9696;
-        System.out.printf("%1$s %1$s", s);
+        System.out.println("Max Value Short " + Short .MAX_VALUE);
+        System.out.println("Min Value Short " + Short.MIN_VALUE);
+        System.out.println("No of Bytes Short " + Short.BYTES);
+        System.out.println("No of Bits Short " + Short.SIZE);
+        
+        System.out.println("Max Value Integer " + Integer.MAX_VALUE);
+        System.out.println("Min Value Integer " + Integer.MIN_VALUE);
+        System.out.println("No of Bytes Integer " + Integer.BYTES);
+        System.out.println("No of Bits Integer " + Integer.SIZE);
 
-        System.out.printf("%1$10d  %2$18f", i, d);
+        System.out.println("Max Value Long " + Long.MAX_VALUE);
+        System.out.println("Min Value Long " + Long.MIN_VALUE);
+        System.out.println("No of Bytes Long " + Long.BYTES);
+        System.out.println("No of Bits Long " + Long.SIZE);
 
-        System.out.printf("%1$s %1$s", s);
 
-        System.out.printf("%1$s %1$s", s);
+        System.out.println("Max Value Float " + Float.MAX_VALUE);
+        System.out.println("Min Value Float " + Float.MIN_VALUE);
+        System.out.println("No of Bytes Float " + Float.BYTES);
+        System.out.println("No of Bits Float " + Float.SIZE);
 
+        System.out.println("Max Value Double " + Double.MAX_VALUE);
+        System.out.println("Min Value Double " + Double.MIN_VALUE);
+        System.out.println("No of Bytes Double " + Double.BYTES);
+        System.out.println("No of Bits Double " + Double.SIZE);
+
+        System.out.println("Max Value Character " + Character.MAX_VALUE);
+        System.out.println("Min Value Character " + Character.MIN_VALUE);
+        System.out.println("No of Bytes Character " + Character.BYTES);
+        System.out.println("No of Bits Character " + Character.SIZE);  
+        
+        // None of four method 
+        // output---> 
+        // Max Value Byte 127
+        // Min Value Byte -128
+        // No of Bytes Byte 1
+        // No of Bits Byte 8
+
+        // Max Value Short 32767
+        // Min Value Short -32768
+        // No of Bytes Short 2
+        // No of Bits Short 16
+
+        // Max Value Integer 2147483647
+        // Min Value Integer -2147483648
+        // No of Bytes Integer 4
+        // No of Bits Integer 32
+
+        // Max Value Long 9223372036854775807
+        // Min Value Long -9223372036854775808
+        // No of Bytes Long 8
+        // No of Bits Long 64
+
+        // Max Value Float 3.4028235E38
+        // Min Value Float 1.4E-45
+        // No of Bytes Float 4
+        // No of Bits Float 32
+
+        // Max Value Double 1.7976931348623157E308
+        // Min Value Double 4.9E-324
+        // No of Bytes Double 8
+        // No of Bits Double 64
+        
+        // Max Value Character ?
+        // Min Value Character
+        // No of Bytes Character 2
+        // No of Bits Character 16 
     }
 }
