919b1f6993ff75dd68465c07475f933ee142df43
shubham409 <sm091274@gmail.com>
2020-11-11 21:30:58 +0530

    string

--- StringFormat.java
+++ StringFormat.java
@@ -3,7 +3,8 @@ public class StringFormat {
     public static void main(String[] args) {
 
         String s= "google";
-        System.out.printf("%s", s);
         
+        System.out.printf("%s", s);
+
     }
 }
